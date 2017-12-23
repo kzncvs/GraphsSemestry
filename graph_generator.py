2c6a8e53bec74a04236f85ade1df4d28b6ba1b49
Sergo <s@kazancev.xyz>
2017-12-24 01:14:30 +0300

    Graphs generator added

--- graph_generator.py
+++ graph_generator.py
@@ -1,2 +1,23 @@
-def generate(vertex_count):
-    pass
+import random
+
+
+def generate(vertex_count, edge_count):
+    inf = float('inf')
+    graph = []
+    for _ in range(vertex_count):
+        graph.append([inf] * vertex_count)
+    for i in range(len(graph)):
+        graph[i][i] = 0
+
+    added = 0
+    while True:
+        x = random.randint(0, vertex_count - 1)
+        y = random.randint(0, vertex_count - 1)
+        if graph[x][y] == inf:
+            new = random.randint(1, 20)
+            graph[x][y] = new
+            graph[y][x] = new
+            added += 1
+        if added == edge_count:
+            break
+    return graph
--- main.py
+++ main.py
@@ -1,27 +1,29 @@
 from algorythms import bellman_ford as bf
 from algorythms import floyd_warshall as fw
 from algorythms import dijkstra as di
+import graph_generator as gg
 
 i = float('inf')
 
-g1 = [[0.0, 7.0, 9.0, i, i, 14],
-      [7.0, 0.0, 10.0, 15.0, i, i],
-      [9.0, 10.0, 0.0, 11.0, i, 2.0],
-      [i, 15.0, 11.0, 0.0, 6.0, i],
-      [i, i, i, 6.0, 0.0, 9.0],
-      [14.0, i, 2.0, i, 9.0, 0.0]]
+# g1 = [[0.0, 7.0, 9.0, i, i, 14],
+#       [7.0, 0.0, 10.0, 15.0, i, i],
+#       [9.0, 10.0, 0.0, 11.0, i, 2.0],
+#       [i, 15.0, 11.0, 0.0, 6.0, i],
+#       [i, i, i, 6.0, 0.0, 9.0],
+#       [14.0, i, 2.0, i, 9.0, 0.0]]
+#
+# g2 = [[0, 1, i, i, i, i, i, i, i],
+#       [1, 0, 1, i, 1, i, i, i, i],
+#       [i, 1, 0, 1, i, 1, i, i, i],
+#       [i, i, 1, 0, i, i, i, i, i],
+#       [i, 1, i, i, 0, 1, 1, i, i],
+#       [i, i, 1, i, 1, 0, i, i, 1],
+#       [i, i, i, i, 1, i, 0, 1, i],
+#       [i, i, i, i, i, i, 1, 1, i],
+#       [i, i, i, i, i, 1, i, i, 0]]
 
-g2 = [[0, 1, i, i, i, i, i, i, i],
-      [1, 0, 1, i, 1, i, i, i, i],
-      [i, 1, 0, 1, i, 1, i, i, i],
-      [i, i, 1, 0, i, i, i, i, i],
-      [i, 1, i, i, 0, 1, 1, i, i],
-      [i, i, 1, i, 1, 0, i, i, 1],
-      [i, i, i, i, 1, i, 0, 1, i],
-      [i, i, i, i, i, i, 1, 1, i],
-      [i, i, i, i, i, 1, i, i, 0]]
+kek = gg.generate(10, 20)
 
-
-print(di.find(g2))
-print(fw.find(g2))
-print(bf.find(g2))
+print(di.find(kek))
+print(fw.find(kek))
+print(bf.find(kek))
